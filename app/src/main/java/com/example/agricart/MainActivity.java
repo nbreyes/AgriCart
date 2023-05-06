0338ae3b5dad1d02ed80ffe39c5e1e93d96a61e3
Neil Reyes <reyesneil211@gmail.com>
2023-05-06 10:33:21 +0800

    Added bottom nav view

--- app/src/main/java/com/example/agricart/MainActivity.java
+++ app/src/main/java/com/example/agricart/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.agricart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.agricart.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+        changeVisibleFragment(new HomeFragment());
+
+        mainBinding.BottomNavView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.home:
+                    changeVisibleFragment(new HomeFragment());
+                    break;
+                case R.id.search:
+                    changeVisibleFragment(new SearchFragment());
+                    break;
+                case R.id.history:
+                    changeVisibleFragment(new HistoryFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    public void changeVisibleFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.FrameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
