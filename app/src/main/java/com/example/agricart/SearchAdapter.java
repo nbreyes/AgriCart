2e38644ff787b43ee6dd7ff1fc363f6b1208b9f0
Neil Reyes <reyesneil211@gmail.com>
2023-05-11 10:46:13 +0800

    Added functions to when a store is clicked in search

--- app/src/main/java/com/example/agricart/SearchAdapter.java
+++ app/src/main/java/com/example/agricart/SearchAdapter.java
@@ -1,12 +1,14 @@
     package com.example.agricart;
 
     import android.content.Context;
+    import android.content.Intent;
     import android.view.LayoutInflater;
     import android.view.View;
     import android.view.ViewGroup;
     import android.widget.TextView;
 
     import androidx.annotation.NonNull;
+    import androidx.cardview.widget.CardView;
     import androidx.recyclerview.widget.RecyclerView;
 
     import java.util.ArrayList;
@@ -31,6 +33,16 @@
         @Override
         public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
             holder.searchStoreDisplay.setText(arrayList.get(position).getStoreName());
+
+            holder.cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(context, StoreClicked.class);
+                    intent.putExtra("storeName", arrayList.get(position).getStoreName());
+                    intent.putExtra("description", arrayList.get(position).getDescription());
+                    context.startActivity(intent);
+                }
+            });
         }
 
         @Override
@@ -42,10 +54,12 @@
         public class MyViewHolder extends RecyclerView.ViewHolder {
 
             TextView searchStoreDisplay;
+            CardView cardView;
 
             public MyViewHolder(@NonNull View itemView) {
                 super(itemView);
                 searchStoreDisplay = itemView.findViewById(R.id.searchStoreDisplay);
+                cardView = itemView.findViewById(R.id.cardView);
             }
         }
 
--- app/src/main/java/com/example/agricart/SearchStore.java
+++ app/src/main/java/com/example/agricart/SearchStore.java
@@ -4,16 +4,23 @@ import com.google.firebase.database.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
 public class SearchStore {
-    String storeName;
+    String storeName, description;
+
+
 
     public SearchStore() {
     }
 
-    public SearchStore(String storeName){
+    public SearchStore(String storeName, String description){
         this.storeName = storeName;
+        this.description = description;
     }
 
     public String getStoreName() {
         return storeName;
     }
+    public String getDescription() {
+        return description;
+    }
 }
+
